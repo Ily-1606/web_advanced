bd52943e56f1aec3435eec77e7dd688f578be7bc
Nguyên Nguyên <no1.ily1606@gmail.com>
2020-11-02 22:38:05 +0700

    Update

---
+++ bai9/index.php
@@ -0,0 +1,24 @@
+<?php 
+// folder de up file len
+$target_path = "uploads/";
+
+if(isset($_FILES['ufile']))
+{
+
+$target_path = $target_path . basename( $_FILES['ufile']['name']); 
+if ( !preg_match('/\.(jpg|gif)$/i',basename($_FILES['ufile']['name'] )) )
+{ echo "Khong phai file anh!";}
+else 
+// xu ly trung ten
+if (file_exists($target_path))
+{
+echo basename( $_FILES['ufile']['name']) . " already exists. ";}
+else 
+if(move_uploaded_file($_FILES['ufile']['tmp_name'], $target_path)) {
+echo "The file ". basename( $_FILES['ufile']['name']). 
+" has been uploaded";
+
+} else{
+echo "There was an error uploading the file, please try again!";
+}
+}
